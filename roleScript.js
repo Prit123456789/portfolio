82d6ce829b63ce2ce5c1982024d98763599a75ed
Prit patel <148608375+Prit123456789@users.noreply.github.com>
2025-05-14 11:08:42 +0530

    Add files via upload

---
+++ roleScript.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "a Full Stack Developer",
+      "a Web Developer",
+      "a UI-UX Designer",,
+      "a Coder",
+      "Prit Patel",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
